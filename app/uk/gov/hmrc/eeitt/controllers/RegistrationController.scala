665fc6de36b01ab1a1626527e48955c3b6361782
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-02-20 21:48:55 +0000

    pattern matching adding to give a different message for user and agents, message for agents stills needs changed to access code.

--- app/uk/gov/hmrc/eeitt/controllers/RegistrationController.scala
+++ app/uk/gov/hmrc/eeitt/controllers/RegistrationController.scala
@@ -99,7 +99,13 @@ trait RegistrationControllerHelper extends BaseController with I18nSupport {
         val response = jsonErrors match {
           // This occurs when registrationNumber is less that 15 characters. We want in such a case
           // return proper response (200) to the client.
-          case (JsPath(KeyPathNode("registrationNumber") :: _), _) :: _ => Ok(INCORRECT_KNOWN_FACTS_BUSINESS_USERS.toJson(messages))
+          case (JsPath(KeyPathNode("registrationNumber") :: _), _) :: _ =>
+            findUser match {
+              case agent: FindUser[RegisterAgentRequest, PostcodeValidator[EtmpAgent]] =>
+                Ok(INCORRECT_KNOWN_FACTS_AGENTS.toJson(messages))
+              case _ =>
+                Ok(INCORRECT_KNOWN_FACTS_BUSINESS_USERS.toJson(messages))
+            }
           case _ => BadRequest(Json.obj("message" -> JsError.toJson(jsonErrors)))
         }
         Future.successful(response)
