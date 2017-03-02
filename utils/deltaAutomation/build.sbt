30e7885880cc0c2dd3b51489faece4449f751dd9
Harrison Baxter <harrison.baxter@digital.hmrc.gov.uk>
2017-03-02 15:11:30 +0000

    added the delta automation to the project

---
+++ utils/deltaAutomation/build.sbt
@@ -0,0 +1,12 @@
+name := "delta-automation"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+
+libraryDependencies ++= Seq(
+  "org.apache.poi" % "poi"              % "3.15-beta2",
+  "org.apache.poi" % "poi-ooxml"        % "3.15-beta2",
+  "org.apache.poi" % "poi-ooxml-schemas"% "3.15-beta2"
+)
---
+++ utils/deltaAutomation/src/main/scala/FileImport.scala
@@ -0,0 +1,90 @@
+import java.io.File
+import java.util.Calendar
+
+import org.apache.poi.poifs.crypt.{Decryptor, EncryptionInfo}
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
+
+
+/**
+  * Created by harrison on 02/03/17.
+  */
+object FileImport {
+  println("Please enter the location of the file as well as the file name including any file extension then a comma then the password:")
+  val fileDetails: Array[String] = readLine.split(",").map(_.toString)
+  val fileLocation: String = fileDetails.head
+  val password: String = fileDetails.tail.head
+  val myWorkbook = importFile(s"$fileLocation", s"$password")
+  val fileAsString = convertFileToString(myWorkbook)
+  val splitAgentOrBusiness: List[Array[String]] = fileAsString.map(f => f.split("\\|"))
+  val agentOrBusinessUser: String = splitAgentOrBusiness.tail.head.head
+  val currentDateTime = Calendar.getInstance.getTime
+
+
+  val filteredFile: List[String] = agentOrBusinessUser match {
+    case "002" => filterAgentUser(fileAsString)
+    case "001" => filterBusinessUser(fileAsString)
+    case _ => null
+  }
+  println("Please enter a file location to output to:")
+  val fileName: String = scala.io.StdIn.readLine()
+  printToFile(new File(s"$fileName$currentDateTime.txt")) { p => filteredFile.foreach(p.println) }
+
+  def filterBusinessUser(fileString: List[String]): List[String] = {
+    val delFirstLine = fileString.tail
+    val splitString = delFirstLine.map(f => f.split("\\|")).filter(f => !f.contains("select"))
+    val parsedData = splitString.map(x => (s"""${x(0)}|${x(1)}|||||||||${x(10)}|${x(11)}"""))
+    parsedData
+  }
+
+  def filterAgentUser(fileString: List[String]): List[String] = {
+    val delFirstLine = fileString.tail
+    val splitString = delFirstLine.map(f => f.split("\\|")).filter(f => !f.contains("select"))
+    val parsedData = splitString.map(x => (s"""${x(0)}|${x(1)}|||||||||${x(10)}|${x(11)}|${x(12)}|||||||||${x(21)}|${x(22)}"""))
+    parsedData
+  }
+
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try {
+      op(p)
+    } finally {
+      p.close()
+    }
+  }
+
+  def convertFileToString(workBook: XSSFWorkbook): List[String] = {
+    val sheet = myWorkbook.getSheetAt(0)
+    val maxNumOfCells = sheet.getRow(0).getLastCellNum
+    val rows = sheet.rowIterator()
+    val rowBuffer: ListBuffer[String] = ListBuffer.empty[String]
+    for (row <- rows) {
+      val cells = row.cellIterator()
+      val listOfCells = for {cell <- 0 to (maxNumOfCells)} yield {
+        if (row.getCell(cell) == null) {
+          ""
+        } else {
+          row.getCell(cell).toString
+        }
+      }
+      rowBuffer += listOfCells.mkString("|")
+    }
+    rowBuffer.toList
+  }
+
+  def importFile(fileLocation: String, password: String): XSSFWorkbook = {
+    val fs = new NPOIFSFileSystem(new File(s"$fileLocation"), true)
+    val info = new EncryptionInfo(fs)
+    val d: Decryptor = Decryptor.getInstance(info)
+
+    if (!d.verifyPassword(s"$password")) {
+      println("unable to process document incorrect password")
+    }
+    val wb: XSSFWorkbook = new XSSFWorkbook(d.getDataStream(fs))
+
+    wb
+  }
+}
