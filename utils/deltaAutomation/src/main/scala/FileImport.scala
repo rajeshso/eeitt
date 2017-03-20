3b0d636abc6df39e4b45f323d14032c7b44708c2
Rajesh Somasundaram <fun_rajesh@yahoo.com>
2017-03-20 14:12:35 +0000

    Refactoring - Exceptions

--- utils/deltaAutomation/src/main/scala/FileImport.scala
+++ utils/deltaAutomation/src/main/scala/FileImport.scala
@@ -1,77 +1,23 @@
-import java.io.{ File, PrintWriter }
-import java.util.Calendar
-import java.nio.file.{ Files, Path, Paths }
+import java.io.{File, PrintWriter}
 import java.nio.file.Files._
+import java.nio.file.{Files, Path, Paths}
+import java.util.Calendar
 
 import com.typesafe.scalalogging.Logger
-import org.apache.poi.poifs.crypt.{ Decryptor, EncryptionInfo }
+import org.apache.poi.poifs.crypt.{Decryptor, EncryptionInfo}
 import org.apache.poi.poifs.filesystem.NPOIFSFileSystem
-import org.apache.poi.ss.usermodel.{ Cell, Row }
-import org.apache.poi.xssf.usermodel.{ XSSFSheet, XSSFWorkbook }
+import org.apache.poi.ss.usermodel.{Cell, Row}
+import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
+import scala.util.{Failure, Success, Try}
 
 trait FileImportTrait {
   var logger = Logger("FileImport")
   type RowString = String
   type CellValue = String
   type CellsArray = Array[CellValue]
-
-  def printToFile(f: File)(op: PrintWriter => Unit) = {
-    val p: PrintWriter = new PrintWriter(f)
-    try {
-      op(p)
-    } catch {
-      case e: Throwable => logger.error(e.getMessage)
-    } finally {
-      logger.info("The output file is " + f.getAbsoluteFile)
-      p.close()
-    }
-  }
-
-  def readRows(workBook: XSSFWorkbook): List[RowString] = {
-    val sheet: XSSFSheet = workBook.getSheetAt(0)
-    val maxNumOfCells: Short = sheet.getRow(0).getLastCellNum
-    val rows: Iterator[Row] = sheet.rowIterator()
-    val rowBuffer: ListBuffer[RowString] = ListBuffer.empty[String]
-    for (row <- rows) {
-      val cells: Iterator[Cell] = row.cellIterator()
-      val listOfCells: IndexedSeq[String] = for { cell <- 0 to (maxNumOfCells) } yield {
-        if (row.getCell(cell) == null) {
-          ""
-        } else {
-          row.getCell(cell).toString
-        }
-      }
-      rowBuffer += listOfCells.mkString("|")
-    }
-    rowBuffer.toList
-  }
-
-  def getPasswordVerifiedFileAsWorkbook(fileLocation: String, password: String): XSSFWorkbook = {
-    val fileSystem: NPOIFSFileSystem = new NPOIFSFileSystem(new File(s"$fileLocation"), true)
-    val encryptionInfo: EncryptionInfo = new EncryptionInfo(fileSystem)
-    val decryptor: Decryptor = Decryptor.getInstance(encryptionInfo)
-    decryptor.verifyPassword(s"$password")
-    new XSSFWorkbook(decryptor.getDataStream(fileSystem))
-  }
-
-  def verifyPassword(file: String, password: String): Boolean = {
-    val fileSystem: NPOIFSFileSystem = new NPOIFSFileSystem(new File(s"$file"), true)
-    val encryptionInfo: EncryptionInfo = new EncryptionInfo(fileSystem)
-    val decryptor: Decryptor = Decryptor.getInstance(encryptionInfo)
-    var verificationSuccessful: Boolean = false
-    try {
-      verificationSuccessful = decryptor.verifyPassword(s"$password")
-      if (!verificationSuccessful) {
-        logger.info("Unable to process document - incorrect password")
-      }
-    } catch {
-      case t: Throwable => logger.error(t.getMessage)
-    }
-    verificationSuccessful
-  }
 
   def isValidFileLocation(fileLocation: String, read: Boolean, write: Boolean): Boolean = {
     val path: Path = Paths.get(fileLocation)
@@ -101,15 +47,61 @@ trait FileImportTrait {
       logger.error(s"Incorrent File Content in $file - The program exits")
       false
     } else {
-      try {
-        new NPOIFSFileSystem(new File(s"$file"), true)
-      } catch {
-        case e: Throwable =>
+      Try(new NPOIFSFileSystem(new File(s"$file"), true)) match {
+        case Success(_) => true
+        case Failure(e) => {
           logger.error(s"Incorrent File Content in $file ${e.getMessage}- The program exits")
-          return false
+          false
+        }
       }
-      true
     }
+  }
+
+  def verifyPassword(file: String, password: String): Boolean = {
+    val fileSystem: NPOIFSFileSystem = new NPOIFSFileSystem(new File(s"$file"), true)
+    val encryptionInfo: EncryptionInfo = new EncryptionInfo(fileSystem)
+    val decryptor: Decryptor = Decryptor.getInstance(encryptionInfo)
+    var verificationSuccessful: Boolean = false
+    Try(decryptor.verifyPassword(password)) match {
+      case Success(true) => {
+        true
+      }
+      case Success(false) => {
+        logger.info("Unable to process document - incorrect password")
+        false
+      }
+      case Failure(throwable) => {
+        logger.error(throwable.getMessage)
+        false
+      }
+    }
+  }
+
+  def getPasswordVerifiedFileAsWorkbook(fileLocation: String, password: String): XSSFWorkbook = {
+    val fileSystem: NPOIFSFileSystem = new NPOIFSFileSystem(new File(s"$fileLocation"), true)
+    val encryptionInfo: EncryptionInfo = new EncryptionInfo(fileSystem)
+    val decryptor: Decryptor = Decryptor.getInstance(encryptionInfo)
+    decryptor.verifyPassword(s"$password")
+    new XSSFWorkbook(decryptor.getDataStream(fileSystem))
+  }
+
+  def readRows(workBook: XSSFWorkbook): List[RowString] = {
+    val sheet: XSSFSheet = workBook.getSheetAt(0)
+    val maxNumOfCells: Short = sheet.getRow(0).getLastCellNum
+    val rows: Iterator[Row] = sheet.rowIterator()
+    val rowBuffer: ListBuffer[RowString] = ListBuffer.empty[String]
+    for (row <- rows) {
+      val cells: Iterator[Cell] = row.cellIterator()
+      val listOfCells: IndexedSeq[String] = for {cell <- 0 to (maxNumOfCells)} yield {
+        if (row.getCell(cell) == null) {
+          ""
+        } else {
+          row.getCell(cell).toString
+        }
+      }
+      rowBuffer += listOfCells.mkString("|")
+    }
+    rowBuffer.toList
   }
 
   def getUser(userIdIndicator: CellValue): User = {
@@ -125,12 +117,12 @@ trait FileImportTrait {
     val formatFunction: (CellsArray) => String
 
     def partitionUserAndNonUserRecords(
-      rowsList: List[RowString],
-      outputFileLocation: String,
-      badFileLocation: String,
-      currentDateTime: String,
-      inputFileName: String
-    ): Unit = {
+                                        rowsList: List[RowString],
+                                        outputFileLocation: String,
+                                        badFileLocation: String,
+                                        currentDateTime: String,
+                                        inputFileName: String
+                                      ): Unit = {
       val rowsListExceptHeader: List[RowString] = rowsList.tail
       val (goodRows, badRows): (List[CellsArray], List[CellsArray]) = rowsListExceptHeader.map(rowString => rowString.split("\\|")).partition(cellArray => !(cellArray(1) == "" || cellArray(1) == "select"))
       val goodRowsList: List[RowString] = goodRows.map(formatFunction)
@@ -144,12 +136,16 @@ trait FileImportTrait {
 
   case object BusinessUser extends User {
     val name: String = "001"
-    override val formatFunction = (cellsArray: CellsArray) => { (s"""${cellsArray(0)}|${cellsArray(1)}|||||||||${cellsArray(10)}|${cellsArray(11)}""") }
+    override val formatFunction = (cellsArray: CellsArray) => {
+      (s"""${cellsArray(0)}|${cellsArray(1)}|||||||||${cellsArray(10)}|${cellsArray(11)}""")
+    }
   }
 
   case object AgentUser extends User {
     val name: String = "002"
-    override val formatFunction = (cellsArray: CellsArray) => { (s"""${cellsArray(0)}|${cellsArray(1)}|||||||||${cellsArray(10)}|${cellsArray(11)}|${cellsArray(12)}|||||||||${cellsArray(21)}|${cellsArray(22)}""") }
+    override val formatFunction = (cellsArray: CellsArray) => {
+      (s"""${cellsArray(0)}|${cellsArray(1)}|||||||||${cellsArray(10)}|${cellsArray(11)}|${cellsArray(12)}|||||||||${cellsArray(21)}|${cellsArray(22)}""")
+    }
   }
 
   case object UnsupportedUser extends User {
@@ -164,6 +160,18 @@ trait FileImportTrait {
   protected def write(outputFileLocation: String, badFileLocation: String, goodRowsList: List[RowString], badRowsList: List[RowString], fileName: String): Unit = {
     printToFile(new File(s"$badFileLocation//$fileName")) { rowString => badRowsList.foreach(rowString.println) }
     printToFile(new File(s"$outputFileLocation//$fileName")) { rowString => goodRowsList.foreach(rowString.println) }
+  }
+
+  def printToFile(f: File)(op: PrintWriter => Unit) = {
+    val p: PrintWriter = new PrintWriter(f)
+    try {
+      op(p)
+      logger.info("The output file is " + f.getAbsoluteFile)
+    } catch {
+      case e: Throwable => logger.error(e.getMessage)
+    } finally {
+      p.close()
+    }
   }
 }
 
@@ -192,21 +200,23 @@ object FileImport extends FileImportTrait {
   }
 
   private def validateInput(
-    inputFileLocation: String,
-    outputFileLocation: String,
-    badFileLocation: String,
-    inputFileName: String,
-    password: String
-  ) = {
+                             inputFileLocation: String,
+                             outputFileLocation: String,
+                             badFileLocation: String,
+                             inputFileName: String,
+                             password: String
+                           ) = {
     if (!isValidFileLocation(inputFileLocation, true, false)) System.exit(0)
-    if (!isValidFileLocation(outputFileLocation, false, true)) System.exit(0)
-    if (!isValidFileLocation(badFileLocation, false, true)) System.exit(0)
-    if (!isValidFile(s"$inputFileLocation//$inputFileName")) System.exit(0)
-    if (!verifyPassword(s"$inputFileLocation//$inputFileName", s"$password")) System.exit(0)
-    logger.info("The input file was:" + inputFileName)
+    else if (!isValidFileLocation(outputFileLocation, false, true)) System.exit(0)
+    else if (!isValidFileLocation(badFileLocation, false, true)) System.exit(0)
+    else if (!isValidFile(s"$inputFileLocation//$inputFileName")) System.exit(0)
+    else if (!verifyPassword(s"$inputFileLocation//$inputFileName", s"$password")) System.exit(0)
+    else
+      logger.info("The input file was:" + inputFileName)
   }
 
   def reInitLogger(testLogger: Logger): Unit = {
+    // Mock Logger
     logger = testLogger
   }
 
