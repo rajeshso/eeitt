e7432c7ce33df4053f4bba115f7be76d4b9c36a3
Harrison Baxter <harrison.baxter@digital.hmrc.gov.uk>
2017-04-05 11:33:20 +0100

    fixed the dry run script

--- utils/deltaAutomation/DryRun.sh
+++ utils/deltaAutomation/DryRun.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sm --start EEITT_ALL -fo
-
 
 USER_TYPE=$1
 INPUTFILE=$2
@@ -9,12 +7,10 @@ INPUTFILE="@$INPUTFILE"
 
 if [ $# -eq 2 ]
  then
-    curl --url "http://localhost:9191/eeitt/etmp-data/dry-run/$USER_TYPE" --user dave:davespassword --header 'x-requested-with: bar' --data-binary $INPUTFILE -v
+    curl --url "https://www-qa.tax.service.gov.uk/eeitt-auth/etmp-data/$USER_TYPE/dry-run" --user eeitt-qa-user:gp6uIbyBTevIf/EB --header 'x-requested-with: bar' --data-binary $INPUTFILE -v
 
-    echo  --url "http://localhost:9191/eeitt/etmp-data/dry-run/$USER_TYPE/" --user dave:davespassword --header 'x-requested-with: bar' --data $INPUTFILE -v
+    echo  --url "https://www-qa.tax.service.gov.uk/eeitt-auth/etmp-data/$USER_TYPE/dry-run" --user eeitt-qa-user:gp6uIbyBTevIf/EB --header 'x-requested-with: bar' --data-binary $INPUTFILE -v
 
  else
     echo "Incorrect number of arguments supplied. The format is ./DryRun.sh RECORD_TYPE FULL_PATH_TO_FILE . For example, ./DryRun.sh business-users /home/rajesh/Applications/sample"
 fi
-
-sm --stop EEITT_ALL
